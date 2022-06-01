4012354fd85420a949d92b24e08dea306524c16a
GabrielVersatec <gabriel.silva@versatec.com.br>
2022-06-01 02:43:01 -0300

    adding database postgres

--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,7 +1,9 @@
+import "reflect-metadata"; // necessary to TYPEORM
 import express, { NextFunction, Request, Response } from 'express';
 import cors from 'cors';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
+import '@shared/typeorm';
 
 const app = express();
 
