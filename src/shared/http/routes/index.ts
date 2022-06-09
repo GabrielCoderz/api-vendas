d28dacb516c4861e66b01a927b539bcc884b0982
GabrielCoderz <biel.s.maia@hotmail.com>
2022-06-09 02:09:58 -0300

    service product has been created

--- src/shared/http/routes/index.ts
+++ src/shared/http/routes/index.ts
@@ -1,6 +1,9 @@
+import productsRouter from '@modules/products/routes/products.routes';
 import { Router, Response, Request } from 'express';
 
 const routes = Router();
+
+routes.use('/products', productsRouter);
 
 routes.get('/', (req: Request, res: Response) => {
   return res.json(
--- src/shared/http/server.ts
+++ src/shared/http/server.ts
@@ -1,19 +1,24 @@
 import "reflect-metadata"; // necessary to TYPEORM
 import express, { NextFunction, Request, Response } from 'express';
+import 'express-async-errors';
 import cors from 'cors';
+import { errors } from 'celebrate';
 import routes from './routes';
 import AppError from '@shared/errors/AppError';
 import '@shared/typeorm';
 
 const app = express();
 
-const PORT = 3000;
+const PORT = 3333;
 const HOST = '0.0.0.0';
 
 app.use(cors());
 app.use(express.json());
 
 app.use(routes);
+
+app.use(errors());
+
 app.use((error: Error, request: Request, response: Response, next: NextFunction) => {
   if(error instanceof AppError) {
     return response.status(error.statusCode).json({
